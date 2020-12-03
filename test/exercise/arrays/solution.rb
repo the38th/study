b2989add5f23a7cc180e635bd6aa308dac28c9f4
the38th <step_al_ul@mail.ru>
2020-12-03 00:06:40 -0800

    arrays exercises solved

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,22 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.first
+        array.each { |el| max = el if el > max }
+        array.map { |el| el.positive? ? max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        binnary_search(array, query, 0, array.size)
+      end
+
+      def binnary_search(array, query, left, right)
+        mid = (left + right) / 2
+        return mid if array[mid] == query
+        return -1 if left == right
+        return binnary_search(array, query, mid + 1, right) if array[mid] < query
+
+        binnary_search(array, query, left, mid)
       end
     end
   end
