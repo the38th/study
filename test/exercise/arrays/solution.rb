7abfd29699413487be33e6035d32f493448bfb65
the38th <step_al_ul@mail.ru>
2020-12-07 05:13:22 -0800

    find_max method added

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,22 +2,27 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = array.first
-        array.each { |el| max = el if el > max }
+        max = find_max(array)
         array.map { |el| el.positive? ? max : el }
       end
 
-      def search(array, query)
-        binnary_search(array, query, 0, array.size)
+      def find_max(array)
+        max = array.first
+        array.each { |el| max = el if el > max }
+        max
       end
 
-      def binnary_search(array, query, left, right)
+      def search(array, query)
+        binary_search(array, query, 0, array.size)
+      end
+
+      def binary_search(array, query, left, right)
         mid = (left + right) / 2
         return mid if array[mid] == query
         return -1 if left == right
-        return binnary_search(array, query, mid + 1, right) if array[mid] < query
+        return binary_search(array, query, mid + 1, right) if array[mid] < query
 
-        binnary_search(array, query, left, mid)
+        binary_search(array, query, left, mid)
       end
     end
   end
